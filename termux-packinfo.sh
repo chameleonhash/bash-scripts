e947c8e0edc97f2ef6e0f77cac222d3b5f4997e6
chameleonhash <81000680+chameleonhash@users.noreply.github.com>
2023-01-01 08:42:19 -0500

    Update termux-packinfo.sh

--- termux-packinfo.sh
+++ termux-packinfo.sh
@@ -3,6 +3,10 @@
 
 trap 'exit 130' INT
 
-pkg install pv -y; 
+pkg upgrade -y;
+pkg install pv -y; pkg install figlet -y;
+clear;
+
+figlet -c "LIST OF TERMUX PACKAGES"
 for pkg in $(apt-cache pkgnames | sort);
 do printf "$pkg - $(apt-cache show $pkg | grep -m 1 "Description:" | cut -c 14-)\n" | pv --rate-limit 5 --quiet --line-mode; done 
