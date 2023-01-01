3e3b689e763221d7c336bfb9d68327a0d4871c2a
chameleonhash <81000680+chameleonhash@users.noreply.github.com>
2023-01-01 12:57:59 -0500

    Update termux-packinfo.sh

--- termux-packinfo.sh
+++ termux-packinfo.sh
@@ -12,4 +12,4 @@ figlet -w 135 -c -f "mono9" "ctrl c to stop"
 figlet -w 135 -c -f "script" "by chameleonhash"
 figlet -w 135 -c -f "bigmono9" "================"
 for pkg in $(apt-cache pkgnames | sort);
-do printf "$pkg - $(apt-cache show $pkg | grep -m 1 "Description:" | cut -c 14-)\n" | pv --rate-limit 3 --quiet --line-mode; done 
+do printf "$pkg - $(apt-cache show $pkg | grep -m 1 "Description:" | cut -c 14-)\n" | pv --rate-limit 5 --quiet --line-mode; done 
