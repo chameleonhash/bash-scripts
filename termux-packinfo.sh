30f46eae690072a26b2a0ba8f2cfdab326340143
chameleonhash <81000680+chameleonhash@users.noreply.github.com>
2023-01-01 08:30:10 -0500

    Update termux-packinfo.sh

--- termux-packinfo.sh
+++ termux-packinfo.sh
@@ -3,5 +3,6 @@
 
 trap 'exit 130' INT
 
+pkg install pv -y; 
 for pkg in $(apt-cache pkgnames | sort);
-do printf "$pkg - $(apt-cache show $pkg | grep -m 1 "Description:" | cut -c 14-)\andkn>
+do printf "$pkg - $(apt-cache show $pkg | grep -m 1 "Description:" | cut -c 14-)\n" | pv --rate-limit 5 --quiet --line-mode; done 
