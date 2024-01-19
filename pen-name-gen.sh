7f4f0954afba5ff9e522f8ee22c847295ab28206
chameleonhash <chameleonhash@gmail.com>
2024-01-19 01:09:43 -0500

    Update pen-name-gen.sh

--- pen-name-gen.sh
+++ pen-name-gen.sh
@@ -4,7 +4,7 @@ echo "Welcome to the Pen Name Generator!"
 
 # URLs to external files containing male and female first names, and last names
 maleFirstNamesURL="https://example.com/male_first_names.txt"
-femaleFirstNamesURL="https://example.com/female_first_names.txt"
+femaleFirstNamesURL="https://raw.githubusercontent.com/chameleonhash/bash-scripts/main/female.txt"
 lastNamesURL="https://example.com/last_names.txt"
 
 # Function to download names from a URL into an array
